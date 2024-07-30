3aed406a713a4ce4ee3858d58464a82ca4845bd8
Fersss <ferr.siqueiros@gmail.com>
2024-07-30 17:31:35 -0600

    moving stuff around

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 // .eleventy.js
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const baseUrl = process.env.NODE_ENV === "production" ? "/lvl-up/" : "/";
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc,{
-    components: "_includes/webc/*.webc"
+    components: "_includes/webc/**/*.webc"
   });
 
   eleventyConfig.setTemplateFormats(["webc", "html", "md", "njk"]);
@@ -13,15 +14,9 @@ module.exports = function (eleventyConfig) {
     return (data) => `${data.page.fileSlug}/index.html`;
   });
 
-  eleventyConfig.addPassthroughCopy("input/img");
-    // Set a custom base URL for production
-  
-  if (process.env.NODE_ENV === "production") {
-    eleventyConfig.addGlobalData("baseUrl", "/lvl-up/");
-  } else {
-    eleventyConfig.addGlobalData("baseUrl", "/");
-  }
+  eleventyConfig.addGlobalData("baseUrl", baseUrl);
 
+  eleventyConfig.addPassthroughCopy("input/img");
 
   // Ensure no nested directories
   return {
